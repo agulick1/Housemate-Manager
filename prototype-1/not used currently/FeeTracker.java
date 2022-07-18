c513fe0eb7c81e8b4a44ae51ee08077e56389a72
Adam Gulick <109553015+agulick1@users.noreply.github.com>
2022-07-18 17:02:45 -0400

    Delete prototype-1 directory

--- prototype-1/not used currently/FeeTracker.java
+++
@@ -1,9 +0,0 @@
-import java.util.ArrayList;
-import java.util.List;
-public class FeeTracker {
-    List<Debt> debtList;
-    public FeeTracker() {
-        debtList = new ArrayList<>();
-    }
-    // other methods to be added pending update of class diagram
-}
